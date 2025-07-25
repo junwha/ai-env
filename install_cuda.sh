bbf650f44930cd6bf361622cd89a13e35443d45f
Junwha Hong <qbit@unist.ac.kr>
2025-07-25 17:38:41 +0900

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -6,10 +6,8 @@ cd $DOWNLOAD_DIR
 # sudo apt install nvidia-driver-570
 
 # CUDA
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update -y
-sudo apt-get -y install cuda-toolkit-12-6
+wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
+sudo sh cuda_12.6.0_560.28.03_linux.run
 
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run # For later use
 # sudo sh cuda_11.8.0_520.61.05_linux.run
