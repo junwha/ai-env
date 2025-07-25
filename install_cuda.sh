320e8b664198efc37961b4a1239712b9b56352aa
Junwha Hong <qbit@unist.ac.kr>
2025-07-25 17:35:13 +0900

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -8,7 +8,7 @@ cd $DOWNLOAD_DIR
 # CUDA
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get -y install cuda-toolkit-12-6
 
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run # For later use
