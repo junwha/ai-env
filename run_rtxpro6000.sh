2ad4a62b75595c2cb2e920632f1f005159d2fb11
Junwha Hong <qbit@unist.ac.kr>
2026-06-10 16:30:03 +0900

    Update and rename run_rtx6000.sh to run_rtxpro6000.sh

--- run_rtxpro6000.sh
+++ run_rtxpro6000.sh
@@ -3,6 +3,6 @@ DOWNLOAD_DIR=/pre
 mkdir -p $DOWNLOAD_DIR
 
 bash ./install_utils.sh $DOWNLOAD_DIR
-bash ./install_cuda.sh $DOWNLOAD_DIR
+# bash ./install_cuda.sh $DOWNLOAD_DIR
 bash ./install_docker.sh $DOWNLOAD_DIR
 bash ./install_ai_toolkits.sh $HUGGINGFACE_TOKEN $DOWNLOAD_DIR
